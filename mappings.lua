e36c299d82c0eff39f6f57cc8d24aec07895956a
Layton <caolei6767@gmail.com>
2023-08-12 21:38:56 +0800

    update

--- mappings.lua
+++ mappings.lua
@@ -4,53 +4,53 @@
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
 return {
-  -- first key is the mode
-  n = {
-    -- second key is the lefthand side of the map
+	-- first key is the mode
+	n = {
+		-- second key is the lefthand side of the map
+		["<leader>h"] = false,
+		-- navigate buffer tabs with `H` and `L`
+		-- L = {
+		--   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+		--   desc = "Next buffer",
+		-- },
+		-- H = {
+		--   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+		--   desc = "Previous buffer",
+		-- },
 
-    -- navigate buffer tabs with `H` and `L`
-    -- L = {
-    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-    --   desc = "Next buffer",
-    -- },
-    -- H = {
-    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-    --   desc = "Previous buffer",
-    -- },
+		-- Base
+		L = { "$", desc = "Jump end of line" },
+		H = { "^", desc = "Jump end of line" },
 
-    -- Base
-    L = { "$", desc = "Jump end of line" },
-    H = { "^", desc = "Jump end of line" },
+		["<leader>bn"] = { "<cmd>Neotree buffers toggle<cr>", desc = "Toggle buffers" },
 
-    ["<leader>bn"] = { "<cmd>Neotree buffers toggle<cr>", desc = "Toggle buffers" },
+		-- Hop
+		["<leader>w"] = { "<Cmd>HopWord<CR>", desc = "jump: Goto word" },
+		["<leader>j"] = { "<Cmd>HopLine<CR>", desc = "jump: Goto line" },
+		["<leader>k"] = { "<Cmd>HopLine<CR>", desc = "jump: Goto line" },
+		["<leader>c"] = { "<Cmd>HopChar1<CR>", desc = "jump: Goto one char" },
+		["<leader>cc"] = { "<Cmd>HopChar2<CR>", desc = "jump: Goto two chars" },
 
-    -- Hop
-    ["<leader>w"] = { "<Cmd>HopWord<CR>", desc = "jump: Goto word" },
-    ["<leader>j"] = { "<Cmd>HopLine<CR>", desc = "jump: Goto line" },
-    ["<leader>k"] = { "<Cmd>HopLine<CR>", desc = "jump: Goto line" },
-    ["<leader>c"] = { "<Cmd>HopChar1<CR>", desc = "jump: Goto one char" },
-    ["<leader>cc"] = { "<Cmd>HopChar2<CR>", desc = "jump: Goto two chars" },
+		-- suda
+		["<C-x>s"] = { "<Cmd>SudaWrite<CR>", desc = "Suda write" },
+	},
+	v = {
+		-- Base
+		L = { "$", desc = "Jump end of line" },
+		H = { "^", desc = "Jump end of line" },
+		[">"] = { ">gv", desc = "Increase indent" },
+		["<"] = { "<gv", desc = "Increase indent" },
+		["p"] = { "pgvy", desc = "Better past" },
 
-    -- suda
-    ["<C-x>s"] = { "<Cmd>SudaWrite<CR>", desc = "Suda write" },
-  },
-  v = {
-    -- Base
-    L = { "$", desc = "Jump end of line" },
-    H = { "^", desc = "Jump end of line" },
-    [">"] = { ">gv", desc = "Increase indent" },
-    ["<"] = { "<gv", desc = "Increase indent" },
-    ["p"] = { "pgvy", desc = "Better past" },
-
-    -- Hop
-    ["<leader>w"] = { "<Cmd>HopWord<CR>", desc = "jump: Goto word" },
-    ["<leader>j"] = { "<Cmd>HopLine<CR>", desc = "jump: Goto line" },
-    ["<leader>k"] = { "<Cmd>HopLine<CR>", desc = "jump: Goto line" },
-    ["<leader>c"] = { "<Cmd>HopChar1<CR>", desc = "jump: Goto one char" },
-    ["<leader>cc"] = { "<Cmd>HopChar2<CR>", desc = "jump: Goto two chars" },
-  },
-  t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
-  },
+		-- Hop
+		["<leader>w"] = { "<Cmd>HopWord<CR>", desc = "jump: Goto word" },
+		["<leader>j"] = { "<Cmd>HopLine<CR>", desc = "jump: Goto line" },
+		["<leader>k"] = { "<Cmd>HopLine<CR>", desc = "jump: Goto line" },
+		["<leader>c"] = { "<Cmd>HopChar1<CR>", desc = "jump: Goto one char" },
+		["<leader>cc"] = { "<Cmd>HopChar2<CR>", desc = "jump: Goto two chars" },
+	},
+	t = {
+		-- setting a mapping to false will disable it
+		-- ["<esc>"] = false,
+	},
 }
--- options.lua
+++ options.lua
@@ -20,33 +20,33 @@
 --   },
 -- }
 -- If you need more control, you can use the function()...end notation
-local global = require "user.global"
+local global = require("user.global")
 return function(local_vim)
-  local_vim.opt.relativenumber = true
-  local_vim.g.mapleader = " "
-  local_vim.opt.whichwrap = vim.opt.whichwrap - { "b", "s" } -- removing option from list
-  local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
+	local_vim.opt.relativenumber = true
+	local_vim.g.mapleader = " "
+	local_vim.opt.whichwrap = vim.opt.whichwrap - { "b", "s" } -- removing option from list
+	local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
 
-  if global.is_mac then
-    vim.g.clipboard = {
-      name = "macOS-clipboard",
-      copy = { ["+"] = "pbcopy", ["*"] = "pbcopy" },
-      paste = { ["+"] = "pbpaste", ["*"] = "pbpaste" },
-      cache_enabled = 0,
-    }
-  elseif global.is_wsl then
-    vim.g.clipboard = {
-      name = "win32yank-wsl",
-      copy = {
-        ["+"] = "win32yank.exe -i --crlf",
-        ["*"] = "win32yank.exe -i --crlf",
-      },
-      paste = {
-        ["+"] = "win32yank.exe -o --lf",
-        ["*"] = "win32yank.exe -o --lf",
-      },
-      cache_enabled = 0,
-    }
-  end
-  return local_vim
+	if global.is_mac then
+		vim.g.clipboard = {
+			name = "macOS-clipboard",
+			copy = { ["+"] = "pbcopy", ["*"] = "pbcopy" },
+			paste = { ["+"] = "pbpaste", ["*"] = "pbpaste" },
+			cache_enabled = 0,
+		}
+	elseif global.is_wsl then
+		vim.g.clipboard = {
+			name = "win32yank-wsl",
+			copy = {
+				["+"] = "win32yank.exe -i --crlf",
+				["*"] = "win32yank.exe -i --crlf",
+			},
+			paste = {
+				["+"] = "win32yank.exe -o --lf",
+				["*"] = "win32yank.exe -o --lf",
+			},
+			cache_enabled = 0,
+		}
+	end
+	return local_vim
 end
